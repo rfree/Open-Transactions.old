b012e4a751b8741dcd957e6656a936aace40d080
rfreeman <rfree@gmx.us>
2013-11-29 08:40:54 +0100

    scripts to run/start in devel/normal mode

---
+++ src/othint/run-devel.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# please install here a wrapper script or link to g++(>=4.7.2 ?) or clang(>=3.3 ?)
+COMPILER="$HOME/.local/bin/compile-cxx"
+
+# TODO: LEGAL: for imaginary property and licencing law reasons, do not even -lreadline (link it) when disabling GNU with -DOT_ALLOW_GNU_LIBRARIES=0
+# otherwise it will be linked anyway (though not use)
+$COMPILER -std=c++11 -Wfatal-errors -DOT_ALLOW_GNU_LIBRARIES=1 main.cpp -g3 -O0 -o othint -ledit -lcurses && echo "Starting program:" && bash start-devel.sh ; echo "Done."
+
+#-lreadline
